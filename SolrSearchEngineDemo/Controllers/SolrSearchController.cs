73e2270a0046e656f0a868cebff1c3cd182b34a8
James Strassburg <JStrassburg@gmail.com>
2013-04-12 21:37:29 -0500

    Refactoring search controller for readability

--- SolrSearchEngineDemo/Controllers/SolrSearchController.cs
+++ SolrSearchEngineDemo/Controllers/SolrSearchController.cs
@@ -5,7 +5,6 @@ using SolrSearchEngineDemo.Models;
 using System.Collections.Generic;
 using System.Configuration;
 using System.Linq;
-using System.Text.RegularExpressions;
 using System.Web.Http;
 
 namespace SolrSearchEngineDemo.Controllers
@@ -24,52 +23,65 @@ namespace SolrSearchEngineDemo.Controllers
 		// GET api/solrsearch/category/?q=turkey
 		public SearchResult Get(string q, string categoryFilter)
 		{
-			var query = new SolrQuery(Sanitize(q));
+			var query = new SolrQuery(SolrQuerySanitizer.Sanitize(q));
+			var queryOptions = CreateQueryOptions(categoryFilter);
 
+			var solrResult = _solrOperations.Query(query, queryOptions);
+
+			return CreateSearchResultFromSolrResult(solrResult);
+		}
+
+		private static SearchResult CreateSearchResultFromSolrResult(ISolrQueryResults<SearchResultItem> solrResult)
+		{
+			return new SearchResult
+				{
+					Items = solrResult,
+					NumFound = solrResult.NumFound,
+					Categories = solrResult.FacetFields["CategoryName"].Select(
+						x => new KeyValuePair<string, int>(x.Key, x.Value))
+				};
+		}
+
+		private static QueryOptions CreateQueryOptions(string categoryFilter)
+		{
+			return new QueryOptions
+				{
+					Start = 0,
+					Rows = 5,
+					FilterQueries = CreateFilterQueries(categoryFilter),
+					Facet = CreateFacetParameters(),
+					ExtraParams = CreateExtraParams()
+				};
+		}
+
+		private static Dictionary<string, string> CreateExtraParams()
+		{
+			return new Dictionary<string, string>
+			{
+				{ "defType", "dismax" },
+				{ "pf", ConfigurationManager.AppSettings["SolrPhraseFields"]}
+			};
+		}
+
+		private static FacetParameters CreateFacetParameters()
+		{
+			return new FacetParameters
+			{
+				Queries = new List<ISolrFacetQuery>
+				{
+					new SolrFacetFieldQuery("CategoryName") { MinCount = 1 }
+				}
+			};
+		}
+
+		private static ICollection<ISolrQuery> CreateFilterQueries(string categoryFilter)
+		{
 			var filterQueries = new List<ISolrQuery>();
 			if (!string.IsNullOrEmpty(categoryFilter))
 			{
 				filterQueries.Add(new SolrQueryByField("CategoryName", categoryFilter));
 			}
-
-			var solrResult = _solrOperations.Query(
-				query,
-				new QueryOptions
-				{
-					Start = 0,
-					Rows = 5,
-					FilterQueries = filterQueries,
-					Facet = new FacetParameters
-					{
-						Queries = new List<ISolrFacetQuery>
-						{
-							new SolrFacetFieldQuery("CategoryName") { MinCount = 1 }
-						}
-					},
-					ExtraParams = new Dictionary<string, string>
-					{
-						{ "defType", "dismax" },
-						{ "pf", ConfigurationManager.AppSettings["SolrPhraseFields"]}
-					}
-				});
-
-			return new SearchResult
-			{
-				Items = solrResult,
-				NumFound = solrResult.NumFound,
-				Categories = solrResult.FacetFields["CategoryName"].Select(
-					x => new KeyValuePair<string, int>(x.Key, x.Value))
-			};
-		}
-
-		private static readonly Regex SolrSanitizationPattern = new Regex(
-			@"\+|\-|!|\(|\)|\{|\}|\[|\]|\^|~|\*|\?|:|;|&", RegexOptions.Compiled);
-
-		private static string Sanitize(string input)
-		{
-			return string.IsNullOrWhiteSpace(input) ?
-				input :
-				SolrSanitizationPattern.Replace(input, string.Empty);
+			return filterQueries;
 		}
 	}
 }
---
+++ SolrSearchEngineDemo/SolrQuerySanitizer.cs
@@ -0,0 +1,17 @@
+﻿using System.Text.RegularExpressions;
+
+namespace SolrSearchEngineDemo
+{
+	public static class SolrQuerySanitizer
+	{
+		private static readonly Regex SolrSanitizationPattern = new Regex(
+			@"\+|\-|!|\(|\)|\{|\}|\[|\]|\^|~|\*|\?|:|;|&", RegexOptions.Compiled);
+
+		public static string Sanitize(string input)
+		{
+			return string.IsNullOrWhiteSpace(input) ?
+				input :
+				SolrSanitizationPattern.Replace(input, string.Empty);
+		}
+	}
+}
